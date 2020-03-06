d7145d13132d79aabd27951b87e7e5dc1c9855fa
fido2603 <fido2603gamer@gmail.com>
2020-03-06 12:08:41 +0100

    More compact non_recursive with no array algorithm

--- Fibonacci_nonrecursive.py
+++ Fibonacci_nonrecursive.py
@@ -2,16 +2,14 @@ def fib(n):
     # Create array with the first two instances of the Fibonacci sequence
     # We use this array to slowly add the values we figure out, so that we only have to look two values behind
     a = [0, 1]
-    lastnum = None  # Variable used for the final number found
     if n == 0:
-        lastnum = a[0]  # If the number entered is 0, return first index of array (0)
+        return a[0]  # If the number entered is 0, return first index of array (0)
     elif n == 1:
-        lastnum = a[1]  # If the number entered is 1, return the second index of array (1)
+        return a[1]  # If the number entered is 1, return the second index of array (1)
     else:
         for n in range(1, n):  # Does the following for every number from 1 to n
-            lastnum = a[-1] + a[-2]  # Add the newest index in the array to the second newest index
-            a.append(lastnum)  # Add the new value to the array
-    return lastnum
+            a.append(a[-1] + a[-2])  # Add the newest index in the array to the second newest index to the array
+        return a[-1]  # Return newest index in the array
 
 
 def fib_noarray(n):
